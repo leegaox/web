4549dec1a9f0112f95eca1b82b96a3324bb98317
gaox <gaox@deepblueai.com>
2020-03-06 13:18:04 +0800

    集成Swagger2

---
+++ lee/spring/src/main/java/cn/lee/spring/config/Swagger2.java
@@ -0,0 +1,41 @@
+package cn.lee.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * http://localhost:8080/swagger-ui.html
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                 //设置basePackage会将包下的所有被@Api标记类的所有方法作为api
+                .apis(RequestHandlerSelectors.basePackage("cn.lee.spring.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .version("1.0")
+                .termsOfServiceUrl("/")
+                .description("Api 描述")
+                .build();
+    }
+}
--- lee/spring/src/main/java/cn/lee/spring/controller/UserController.java
+++ lee/spring/src/main/java/cn/lee/spring/controller/UserController.java
@@ -4,6 +4,8 @@ import cn.lee.spring.mapper.UserMapper;
 import cn.lee.spring.pojo.Result;
 import cn.lee.spring.pojo.User;
 import cn.lee.spring.util.GsonUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+
 //该注解是 @Controller 和 @ResponseBody 注解的合体版，默认类中的方法返回值都会以json的格式写进响应体中。
 @RestController
 @RequestMapping(value = "/api/user/")
+@Api(tags = "用户接口")
 public class UserController extends BaseController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -34,6 +38,7 @@ public class UserController extends BaseController {
      */
     //接口路径配置
     @GetMapping(value ="getInfo")
+    @ApiOperation(value = "获取用户列表")
     public Result getAllUser(){
         // 首先判断缓存中是否有数据
         List<User> users = (List<User>) redisTemplate.boundValueOps("lee:springboot:users").get();
@@ -58,7 +63,8 @@ public class UserController extends BaseController {
      * @param id
      * @return
      */
-    @RequestMapping(value = "find/{id}")
+    @GetMapping(value = "find/{id}")
+    @ApiOperation(value = "根据id查找用户")
     public Result findUserById(@PathVariable Integer id){
         User user = userMapper.findUserById(id);
         if(user!=null)
@@ -75,6 +81,7 @@ public class UserController extends BaseController {
      */
     //RequestParam获取请求体中的参数
     @PostMapping(value = "addUser")
+    @ApiOperation(value = "添加新用户")
     public Result addUser(@RequestParam(value = "user", required = true) User user){
         if(userMapper.addUser(user)){
             return genSuccessResult(true);
@@ -87,8 +94,9 @@ public class UserController extends BaseController {
      * 更新
      * @param user
      */
-    @RequestMapping(value = "update",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "update",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
+    @ApiOperation(value = "更新用户信息")
     public Result updateUser(@RequestBody @Valid User user , BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             return genErrorResult(-1,bindingResult.getFieldError().getDefaultMessage());
@@ -105,7 +113,8 @@ public class UserController extends BaseController {
      * 刪除
      * @param id
      */
-    @RequestMapping("delete/{id}")
+    @GetMapping("delete/{id}")
+    @ApiOperation(value = "删除用户")
     public Result deleteUser(@PathVariable(value = "id",required = true) Integer id){
         if(userMapper.deleteUserById(id)){
             return genSuccessResult(true);
